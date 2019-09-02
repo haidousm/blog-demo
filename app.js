93c27f35d9aead22cfafe8f31464a0fa6ed00a32
haidousm <haidous.m@gmail.com>
2019-09-02 11:21:12 +0300

    implement home, about, and contact GET routes

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-//jshint esversion:6
-
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
@@ -12,22 +10,35 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(express.static("public"));
 
+app.get("/", (req, res) => {
 
+  res.render("home.ejs", {
+    homeStartingContent: homeStartingContent
+  });
 
+})
 
+app.get("/about", (req, res) => {
 
+  res.render("about.ejs", {
+    aboutContent: aboutContent
+  });
 
+})
 
+app.get("/contact", (req, res) => {
 
+  res.render("contact.ejs", {
+    contactContent: contactContent
+  });
 
+})
 
-
-
-
-
-app.listen(3000, function() {
+app.listen(3000, function () {
   console.log("Server started on port 3000");
 });
